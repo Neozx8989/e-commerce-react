fdbf2723687c338b5f36706b9a4feadd934c6f85
Morning <tushimotot0721@gmail.comgit config --global user.name Neozx8989egit config --global user.name Neozx8989>
2023-01-19 11:50:53 +0800

    detail-page-completed

--- e-commerce-react-project/src/home/detailpage/DetailPageApp.js
+++ e-commerce-react-project/src/home/detailpage/DetailPageApp.js
@@ -7,11 +7,14 @@ function DetailPageApp() {
         return (
             <DetailPageFunc 
                 productImage={product.productImage}
+                productImage2={product.productImage2}
                 name={product.name}
                 price={product.price}
                 check={product.check}
                 add={product.add}
                 buy={product.buy}
+                heart={product.heart}
+                facebookicon={product.facebookicon}
             />
         )
     })
